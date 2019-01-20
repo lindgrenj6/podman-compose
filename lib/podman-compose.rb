891610085565b3a3931f1c54da5112c9f4a5c51f
Jacob Lindgren <jlindgre@redhat.com>
2019-01-20 11:52:17 -0600

    added image extraction and service name resolution

--- lib/podman-compose.rb
+++ lib/podman-compose.rb
@@ -2,28 +2,31 @@
 require 'yaml'
 require 'optparse'
 require 'logger'
+require 'pry'
+require 'pp'
 
 # Using a module to set up a namespace
 module Podman
   # Heres where the meat is
   class Compose
     def initialize(args)
-      @cli_string = ''
+      @cli_strings = {}
       @logger = Logger.new(STDOUT)
-      options = {}
+      @options = {}
 
       # Parse my arguments
+      # THIS ISN'T WORKING YET. I'LL GET TO IT WHEN I GET BASE FUNCTIONALITY WORKING.
       OptionParser.new do |opts|
         opts.banner = 'Usage: podman-compose cmd [options]'
 
-        opts.on('-v', String, 'Show verbose output') do |_v|
+        opts.on('-v', 'Show verbose output') do |_v|
           @logger.level = Logger::DEBUG
         end
-        opts.on('-f', String, 'Specify which compose file to run (default is ./docker-compose.yml)') do |v|
-          options[:file] = v
+        opts.on('-f', 'Specify which compose file to run (default is ./docker-compose.yml)') do |v|
+          @options[:file] = v
         end
         opts.on('-w', 'Watch logs for services') do |_w|
-          options[:watch] = true
+          @options[:watch] = true
         end
         opts.on('-h', '--help', 'Prints this help') do
           puts opts
@@ -31,8 +34,8 @@ module Podman
         end
 
         # set up my help message for later if necessary
-        options[:help] = opts.help
-      end.parse!
+        @options[:help] = opts.help
+      end.parse!(into: @options)
 
       case args.first.downcase
       when 'up'
@@ -42,7 +45,7 @@ module Podman
       when 'build'
         @cmd = :build
       else
-        puts options[:help]
+        puts @options[:help]
         exit
       end
     end
@@ -50,12 +53,45 @@ module Podman
     def execute
       case @cmd
       when :up
-        puts "You put #{:up}"
+        # Read in the compose.yml file, either specified via cli arg or just using the default
+        compose_file = YAML.load_file(@options[:file].nil? ? "./docker-compose.yml" : @options[:file])
+        # Symbolize the keys just to make life a bit easier
+        compose_file = symbolize_keys(compose_file)
+
+        # Go through each of the services, creating a "podman run" command
+        compose_file[:services].keys.each do |service_name|
+          service = compose_file[:services][service_name.to_sym]
+          @cli_strings[service_name] = []
+          @cli_strings[service_name] << "--name #{service_to_container_name(service_name)}"
+
+          @cli_strings[service_name] << service[:image]
+        end
+
+        # Map the keys to super nice command strings to run
+        @cli_strings.transform_values! { |cmd| "podman run -d #{cmd.join(" ")}" }
       when :down
+        compose_file[:services].keys.each do |service_name|
+          @cli_strings[service_name] = []
+          @cli_strings[service_name] << service_to_container_name(service_name)
+        end
+
+        # Map the keys to super nice command strings to run
+        @cli_strings.transform_values! { |cmd| "podman stop #{cmd.join(" ")}" }
       end
+
+      pp @cli_strings
     end
 
     private
 
+    # Get the name for the container in the form "dir_containername"
+    def service_to_container_name(service_name)
+      [File.basename(Dir.getwd), service_name].join("_")
+    end
+
+    # If I had rails I wouldn't have had to copy/paste this BS. But so be it.
+    def symbolize_keys(hash)
+      Hash[ hash.map { |k,v| v.is_a?(Hash) ? [k.to_sym, symbolize_keys(v)] : [k.to_sym,v] } ]
+    end
   end
 end
